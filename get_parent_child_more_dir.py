e9f357c31b9e99559cd00fde79aeaff6e9c4524a
achilles_xushy <yuqingxushiyin@gmail.com>
2017-11-10 10:40:37 +0800

    2. 对比总平台与甘肃平台的目录get_parent_child_more_dir.py

--- get_parent_child_more_dir.py
+++ get_parent_child_more_dir.py
@@ -18,8 +18,12 @@ import time
 from tools import parameters_parser
 
 from tools.ssh_connection import SSHConnect
+from tools.tool_functions import get_cmd_str, file_name_yield, file_key_name_yield, write_record_to_file
+from tools.tool_functions import find_cmd_dir_str, find_cmd_str, find_diff_item_in_lists
 
-PARENT_MORE_FIR = 'parent_more_child.txt'
+
+PARENT_MORE_FIR = 'parent_more_child_dir.txt'
+EXCEPT_LIST = ['adok', ]
 
 
 def main():
@@ -44,7 +48,33 @@ def main():
         both_con = 0
 
     if both_con:
-        pass
+        tianhua_m_list = []
+        m_p_dir_list_cmd = get_cmd_str(parameters_parser.para_dict['parent_node']['media_root_dir'])
+        o_in_th, o_out_th, o_err_th = ssh_con_th.exec_command(m_p_dir_list_cmd)
+        if o_in_th is not None:
+            top_dir_name_list = [i for i in file_name_yield(o_out_th) if i not in EXCEPT_LIST]
+            for i_d in top_dir_name_list:
+                m_p_sub_dir_list_cmd = get_cmd_str(parameters_parser.para_dict['parent_node']['media_root_dir'], i_d)
+                o_in_th1, o_out_th1, o_err_th1 = ssh_con_th.exec_command(m_p_sub_dir_list_cmd)
+                if o_out_th1 is not None:
+                    sub_dir_name_list = [i for i in file_name_yield(o_out_th1)]
+                    tianhua_m_list.extend(sub_dir_name_list)
+                else:
+                    print('command <{}> get file list failed'.format(m_p_sub_dir_list_cmd))
+                    sys.exit(-1)
+
+            m_file_list_cmd = get_cmd_str(parameters_parser.para_dict['child_node']['media_root_dir'])
+            o_stdin, o_stdout, o_stderr = ssh_con_gs.exec_command(m_file_list_cmd)
+            if o_stdout is not None:
+                m_sub_name_list = [i for i in file_name_yield(o_stdout)]
+            else:
+                print('command <{}> get file list failed'.format(m_file_list_cmd))
+                sys.exit(-1)
+            for i_name in tianhua_m_list:
+                if i_name not in m_sub_name_list:
+                    write_record_to_file(PARENT_MORE_FIR, i_name)
+        else:
+            print('command <{}> get file list failed'.format(m_p_dir_list_cmd))
 
     else:
         if is_ok_gs is not True:
